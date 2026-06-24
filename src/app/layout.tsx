1f1d8bdded7f525ac5500b76fe4f683293e51a3f
Noah H <noahraham@gmail.com>
2026-06-24 14:10:46 -0400

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <Background />
         <Navbar />
 
-        <main className="flex-1">
+        <main className="max-w-6xl mx-auto px-6 py-10">
           {children}
         </main>
 
