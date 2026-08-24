ec89a490a49a4604da219b7b96034b27a063e07a
Noah H <noahraham@gmail.com>
2026-08-24 16:45:10 -0400

    s

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,9 @@
 export default function Contact() {
   return (
     <div className="max-w-xl mx-auto mt-20 text-center">
-      <h1 className="text-3xl-text-red-400 font-bold mb-6">Contact
+      <h1 className="text-3xl font-bold mb-6 text-red-400">Contact
         <span className="text-blue-400"> Us</span>
+      </h1>
 
       <p className="text-white mb-6">
         Reach out for sponsorships, collaboration, or questions. We'd love to hear from you!
