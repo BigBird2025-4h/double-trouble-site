f0ba9e7416f232583ed69d2e46f6311d8a5ac4e0
Noah H <noahraham@gmail.com>
2026-08-24 16:40:19 -0400

    fixed join form url

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,17 +3,17 @@ export default function Contact() {
     <div className="max-w-xl mx-auto mt-20 text-center">
       <h1 className="text-3xl font-bold mb-6">Contact Us</h1>
 
-      <p className="text-gray-400 mb-6">
+      <p className="text-white mb-6">
         Reach out for sponsorships, collaboration, or questions. We'd love to hear from you!
       </p>
 
-      <p className="text-gray-400 mb-6">
+      <p className="text-white mb-6">
         Interested in joining? Fill out our form 
         <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSfdl_4pVvgRCfVlBzCo469z4SrniqjKgVdDEMVoCtGJ3m8uug/viewform?usp=sf_link "
+          href="https://forms.gle/we221JcbqTefauhj7"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
+          className="text-white- hover:underline"
         >
           here!
         </a>
