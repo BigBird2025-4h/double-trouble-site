bcee2480936b64ca6de2c0871f501d0b3a5a1979
Noah H <noahraham@gmail.com>
2026-08-31 13:36:25 -0400

    sjbfsjhb

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,13 +6,13 @@ export default function Contact() {
       </h1>
 
       <p className="text-white mb-6">
-        Reach out for sponsorships, collaboration, or questions. We'd love to hear from you!
+        Reach out for sponsorships, collaboration, or questions. We'd love to hear from you! Email at doubletroubleftc@gmail.com or call 423-749-3136.
       </p>
 
       <p className="text-white mb-6">
         Interested in joining? Fill out our form &nbsp;
         <a
-          href="https://forms.gle/we221JcbqTefauhj7"
+          href="https://docs.google.com/forms/d/e/1FAIpQLScOhhXf0-mxo7Gzb8OgqeeWJu6_c34c3Hwn1MBDSP1LNSLOZw/viewform?usp=dialog"
           target="_blank"
           rel="noopener noreferrer"
           className="text-white- hover:text gradient-to-r from-red-500 to-blue-500 font-semibold"
