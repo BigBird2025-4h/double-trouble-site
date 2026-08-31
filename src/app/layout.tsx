dc389d6cb3128a8bbd8bad9d47c344376c38fec8
Noah H <noahraham@gmail.com>
2026-08-31 16:25:48 -0400

    Fixed the site colors and stuff partially

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,20 @@
 import "./globals.css";
+import { Bungee, Barlow } from "next/font/google";
 import Navbar from "../components/Navbar";
 import Background from "../components/Background";
 import Footer from "../components/Footer";
+
+const bungee = Bungee({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bungee",
+});
+
+const barlow = Barlow({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-barlow",
+});
 
 export const metadata = {
   title: "Double Trouble FTC",
@@ -16,11 +29,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col text-white">
+      <body
+        className={`${bungee.variable} ${barlow.variable} min-h-screen flex flex-col text-charcoal bg-cream`}
+      >
         <Background />
         <Navbar />
 
-        <main className="flex-1 w-full max-w-6xl mx-auto px-6 sm:px-10 py-14 sm:py-20">          
+        <main className="flex-1 w-full max-w-6xl mx-auto px-6 sm:px-10 py-14 sm:py-20">
           {children}
         </main>
 
